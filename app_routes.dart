1b15b776094cd7840e2d7d38e0cc023280f7c067
ShahdRabeyWahba <wahbashahdraby@gmail.com>
2025-12-24 10:49:22 +0200

    Add files via upload
    Initialize
    project
    Added apps separately

---
+++ app_routes.dart
@@ -0,0 +1,5 @@
+class AppRoutes {
+  static const String homescreenRoute = 'home_screen';
+  static const String introRoute = 'intro_screen';
+  static const String splashRoute = 'splash_screen';
+}
